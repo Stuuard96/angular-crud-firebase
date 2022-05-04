03e660b07ce66627a8259306aca9fe236c9a3d8c
Stuard Quiroz <stuuard96@gmail.com>
2022-05-03 22:15:26 -0500

    Cambio en el enviroment para que corra en produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBrXH1vqTPveYXR2kKLEh2e35qXkdFCccM",
+    authDomain: "crud-angular-firebase-fdfd2.firebaseapp.com",
+    projectId: "crud-angular-firebase-fdfd2",
+    storageBucket: "crud-angular-firebase-fdfd2.appspot.com",
+    messagingSenderId: "424355055979",
+    appId: "1:424355055979:web:4c80772c08ee5656dbe42e"
+  }
 };
